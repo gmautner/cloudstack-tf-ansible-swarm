f4ff738e011b908e1b1796bb21a3f866c8878b05
Gilberto Mautner <gilberto.mautner@locaweb.com.br>
2025-08-17 17:11:54 -0300

    chore: Clean up S3 Terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,9 +2,5 @@ terraform {
   backend "s3" {
     bucket                      = "gmautner-cluster-2"
     region                      = "us-east-2"
-    endpoint                    = "s3.amazonaws.com"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    force_path_style            = true
   }
 }
