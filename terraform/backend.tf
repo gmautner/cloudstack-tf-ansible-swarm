c3f2436c76d267ea45815bb7d2cbe322e9100191
Gilberto Mautner <gilberto.mautner@locaweb.com.br>
2025-08-17 15:42:42 -0300

    chore: Update Terraform backend configuration with new S3 bucket and region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket                      = "your-terraform-state-bucket" # MUST be globally unique
-#     region                      = "us-east-1"                   # S3 bucket region
-#     endpoint                    = "s3.amazonaws.com"            # S3 endpoint URL
-#     skip_credentials_validation = true
-#     skip_metadata_api_check     = true
-#     force_path_style            = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket                      = "gmautner-cluster-2"
+    region                      = "us-east-2"
+    endpoint                    = "s3.amazonaws.com"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
+}
